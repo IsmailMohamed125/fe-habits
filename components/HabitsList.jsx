d17640faf5ec788dcd11f528607ae3fca94d6f72
ismail125 <im.1369m@gmail.com>
2024-11-26 16:40:37 +0000

    Completed logic update

--- components/HabitsList.jsx
+++ components/HabitsList.jsx
@@ -1,12 +1,11 @@
 import { View, Text, FlatList } from "react-native";
 import Checkbox from "expo-checkbox";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useAuth } from "@clerk/clerk-expo";
 import axiosInstance from "../api";
 
 export default function HabitsList({ habits, setReload }) {
   const [habitsData, setHabitsData] = useState(habits);
-  const [toggleCompletedId, setToggleCompletedId] = useState("");
   const { getToken } = useAuth();
 
   const toggleCheckbox = (id) => {
@@ -14,51 +13,45 @@ export default function HabitsList({ habits, setReload }) {
       // eslint-disable-next-line prettier/prettier
       (habit) => habit._id === id
     );
-    setToggleCompletedId(updatedHabit[0]);
+    const updatedHabits = habitsData.map((habit) =>
+      // eslint-disable-next-line prettier/prettier
+      habit._id === id ? { ...habit, completed: !habit.completed } : habit
+    );
+    setHabitsData(updatedHabits);
+    patchHabitCompleted(updatedHabit[0]);
   };
 
-  useEffect(() => {
+  async function patchHabitCompleted(habit) {
     // eslint-disable-next-line no-undef
     const abortController = new AbortController();
+    try {
+      const token = await getToken();
+      const data = { completed: !habit.completed };
 
-    const patchHabits = async () => {
-      try {
-        const token = await getToken();
-        const data = { completed: !toggleCompletedId.completed };
+      const response = await axiosInstance.patch(`/habit/${habit.id}`, data, {
+        signal: abortController.signal,
+        headers: {
+          Authorization: `Bearer ${token}`, // Replace with your token
+        },
+        // eslint-disable-next-line prettier/prettier
+      });
 
-        const response = await axiosInstance.patch(
-          `/habit/${toggleCompletedId.id}`,
-          data,
-          {
-            signal: abortController.signal,
-            headers: {
-              Authorization: `Bearer ${token}`, // Replace with your token
-            },
-            // eslint-disable-next-line prettier/prettier
-          }
-        );
+      if (response.status === 200) {
+        setReload((cur) => !cur);
+        // setHabits(response.data.data.habits);
+        // setIsLoading(false);
 
-        if (response.status === 200) {
-          setReload((val) => !val);
-          // setHabits(response.data.data.habits);
-          // setIsLoading(false);
-
-          return;
-        } else {
-          throw new Error("Failed to fetch users");
-        }
-      } catch (error) {
-        console.log(error);
-        if (abortController.signal.aborted) {
-          console.log("Data fetching cancelled list");
-        }
+        return;
+      } else {
+        throw new Error("Failed to fetch users");
       }
-    };
-
-    patchHabits();
-
-    return () => abortController.abort("Data fetching cancelled");
-  }, [toggleCompletedId]);
+    } catch (error) {
+      console.log(error);
+      if (abortController.signal.aborted) {
+        console.log("Data fetching cancelled list");
+      }
+    }
+  }
 
   const renderItem = ({ item }) => {
     return (
