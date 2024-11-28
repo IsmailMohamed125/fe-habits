58b9e5fb3a33d57e1daaa1f477bde73187d2d541
root <root@DESKTOP-C5EOE5B>
2024-11-28 15:33:32 +0000

    journal get requests complete

---
+++ app/(home)/journal.jsx
@@ -0,0 +1,83 @@
+import { Text, View, StyleSheet } from "react-native";
+import JournalEntries from "../../components/JournalEntries";
+import JournalEntryById from "../../components/JournalEntryById";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import AddJournalEntry from "../../components/AddJournalEntry";
+import NewEntry from "../../components/NewEntry";
+
+export default function Journal() {
+  const Stack = createStackNavigator();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Journal</Text>
+      </View>
+      <View style={styles.navigationContainer}>
+        <NavigationIndependentTree>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="JournalEntries"
+              screenOptions={{
+                headerShown: false
+              }}
+            >
+              <Stack.Screen name="JournalEntries" component={JournalEntries} />
+              <Stack.Screen
+                name="JournalEntryById"
+                component={JournalEntryById}
+              />
+              <Stack.Screen
+                name="AddJournalEntry"
+                component={AddJournalEntry}
+              />
+              <Stack.Screen name="NewEntry" component={NewEntry} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </NavigationIndependentTree>
+      </View>
+
+      <AddJournalEntry />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2E3239",
+  },
+  header: {
+    width: "100%",
+    padding: 16,
+    backgroundColor: "#2E3239", 
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#AED8FF", 
+  },
+  navigationContainer: {
+    flex: 1,
+    width: "100%",
+  },
+  stackHeader: {
+    backgroundColor: "#2E3239",
+  },
+  stackHeaderText: {
+    fontSize: 18,
+    color: "#2E3239", 
+  },
+  gif: {
+    width: 390,
+    height: 221,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+});
---
+++ components/AddJournalEntry.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function AddJournalEntry() {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate("NewEntry");
+  };
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={handlePress}>
+      <Text style={styles.buttonText}>Add Journal </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#AED8FF",
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 16,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "#2E3239",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
